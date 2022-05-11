3617abc9b17fa38b3ad617889acad518f31b6a9f
TNTMACHO <82248971+TNTMACHO@users.noreply.github.com>
2022-05-11 02:49:06 -0400

    Add files via upload

---
+++ ToDoCell.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoCell.swift
+//  Assignment_List
+//
+//  Created by  Manav Tandon on 5/2/22.
+//
+//
+
+import UIKit
+
+protocol ToDoCellDelegate {
+    func checkMarkTapped(_ sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(self)
+    }
+}
